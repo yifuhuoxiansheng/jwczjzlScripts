7319606fab219121da54391a37575a0e0f31da8d
root <root@vultr.guest>
2017-10-05 08:53:39 +0000

    20171005-

--- insLib4PHP.sh
+++ insLib4PHP.sh
@@ -11,7 +11,15 @@ libpng-devel \
 gd-devel \
 libcurl-devel \
 libmcrypt-devel \
-mhash-devel -y &&\
+mhash-devel \
+git \
+gcc \
+glibc* \
+libso* \
+openssl \
+openssl-devel \
+libnuma.so.1* \
+lsof -y &&\
 wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz &&\
 tar xf libiconv-1.15.tar.gz &&\
 cd libiconv-1.15 &&\
