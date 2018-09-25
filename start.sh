691e182d2c30e5876f3c31f113430bd50d6bd2ce
yeahJwczjzlTest <yeahJwczjzlTest@users.noreply.github.com>
2018-09-25 09:41:30 +0800

    针对性更新一下以前学习时候的开机设置项
    安装 epel 修改为 yum -y install epel-release

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sed s#=enforcing#=disabled#g /etc/selinux/config -i
-chkconfig --list|grep 3:on|awk '{print $1}'|grep -Ev 'sshd|crond|rsyslog|network|udev-post|postfix|sysstat'|awk '{print "chkconfig "$1" off"}'|bash
-echo '>/etc/udev/rules.d/70-persistent-net.rules' >> /etc/rc.local
-echo '*/5 * * * * /usr/sbin/ntpdate pool.ntp.org >/dev/null 2>&1' > /var/spool/cron/root 
-wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
+yum -y install epel-release
+yum -y install curl wget lsof nethogs nmap expect telnet unzip zip htop istop tree gcc 
+#chkconfig --list|grep 3:on|awk '{print $1}'|grep -Ev 'sshd|crond|rsyslog|network|udev-post|postfix|sysstat'|awk '{print "chkconfig "$1" off"}'|bash
+#echo '>/etc/udev/rules.d/70-persistent-net.rules' >> /etc/rc.local
+echo '*/5 * * * * /usr/sbin/ntpdate cn.ntp.org.cn >/dev/null 2>&1' > /var/spool/cron/root 
+
