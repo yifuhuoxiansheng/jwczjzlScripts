9c401b227c32151a12ef3e68fa8ad6530e5a45d7
yeahJwczjzlTest <yeahJwczjzlTest@users.noreply.github.com>
2018-10-17 11:05:28 +0800

    Create yum安装mysql.sh

---
+++ yum安装mysql.sh
@@ -0,0 +1,5 @@
+wget http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum install mysql-community-server -y
+systemctl start mysql
+mysql_secure_installation
