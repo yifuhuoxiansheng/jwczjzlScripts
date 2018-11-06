8f69c7bfa216c91f82dac31c507100c916eee01e
yeahJwczjzlTest <yeahJwczjzlTest@users.noreply.github.com>
2018-11-06 17:59:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # ----------------------------startInstallOS------------------------------------
 sed s#=enforcing#=disabled#g /etc/selinux/config -i
 yum -y install epel-release
-yum -y install curl wget lsof nethogs nmap expect telnet unzip zip htop iotop tree gcc net-tools bash-completion mtr 
+yum -y install curl wget lsof nethogs nmap expect telnet unzip zip htop iotop tree gcc net-tools bash-completion mtr ntpdate
 #chkconfig --list|grep 3:on|awk '{print $1}'|grep -Ev 'sshd|crond|rsyslog|network|udev-post|postfix|sysstat'|awk '{print "chkconfig "$1" off"}'|bash
 #echo '>/etc/udev/rules.d/70-persistent-net.rules' >> /etc/rc.local
 echo '*/5 * * * * /usr/sbin/ntpdate cn.ntp.org.cn >/dev/null 2>&1' > /var/spool/cron/root 
