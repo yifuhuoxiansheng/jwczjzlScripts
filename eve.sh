b1bf492712b0264834ee61e18a11d610a7f8e8df
yeahJwczjzlTest <yeahJwczjzlTest@users.noreply.github.com>
2019-01-03 16:55:37 +0800

    shell 除法
    shell 除法

--- eve.sh
+++ eve.sh
@@ -11,3 +11,18 @@ awk 'BEGIN {print "大于5秒占比:" '$over5000'/'$totalx'}' >> /tmp/QxtOuth/$n
 awk 'BEGIN {print "大于10秒占比:" '$over10000'/'$totalx'}' >> /tmp/QxtOuth/$nowx &&\
 echo "-----------------下面是超时10秒以上的日志-------------------" >> /tmp/QxtOuth/$nowx &&\
 egrep "QxtOuthCheckServiceImpl" /data/logs/biz/source/source.log.$nowx|awk '{match($0,/.+costtime=([0-9]+)/,a)};{if (a[1]>10000) print $1" "$2" "$6" "$10" "$14}' >>/tmp/QxtOuth/$nowx
+
+
+#shell计算中使用除法，基本默认上都是整除。
+#比如：
+#num1=2
+#num2=3
+#num3=`expr $num1 / $num2`
+#这个时候num3=0 ,是因为是因为expr不支持浮点除法
+#解决的方法：
+#num3=`echo "sclae=2; $num1/$num2" | bc`
+#使用bc工具，sclae控制小数点后保留几位
+#还有一种方法
+#awk 'BEGIN{printf "%.2f\n",’$num1‘/’$num2‘}'
+#如果用百分比表示
+#awk 'BEGIN{printf "%.2f%\n",(’$num1‘/’$num2‘)*100}'
