46ccbd9891104f4c687042655f60b3dca961442e
yeahJwczjzlTest <yeahJwczjzlTest@users.noreply.github.com>
2019-05-09 16:50:57 +0800

    Update docker.sh
    安装 docker 以及 docker run 命令行写法

--- docker.sh
+++ docker.sh
@@ -1,3 +1,12 @@
+#yum 安装 docker 并给加速源
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast
+yum -y install docker-ce
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+systemctl restart docker
+
+
 # docker centos 开启 sshd 服务
 
 yum install passwd openssl openssh-server -y
@@ -9,16 +18,15 @@ vi /root/run.sh : /usr/sbin/sshd -D
 # docker 相关:
 
 -------------------- protainer_data
-docker volume create protainer_data
 docker run -p 9000:9000 --name portainer -h portainerServer --restart always -v "/var/run/docker.sock:/var/run/docker.sock" -v portainer_data:/data -d portainer/portainer
 -------------------- mongoDB
-docker run --name mongodb --restart=always -p 27017:27017 -v mongodb_config:/data/configdb -v mongodb_db:/data/db -d docker.io/mongo:3.2
+docker run --name mongodb -h mongoDB --restart=always -p 27017:27017 -v mongodb_config:/data/configdb -v mongodb_db:/data/db -d docker.io/mongo:3.2
 -------------------- mysql
-docker run -p 3306:3306 --name mysql_server --restart=always -v mysql_config:/etc/mysql/conf.d -v mysql_logs:/logs -v mysql_data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Jkxy@mysql123 -d docker.io/mysql:5.7
+docker run -p 3306:3306 --name mysql_server -h mysqlServer --restart=always -v mysql_config:/etc/mysql/conf.d -v mysql_logs:/logs -v mysql_data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Jkxy@mysql123 -d docker.io/mysql:5.7
 -------------------- rabbitMQ
-docker run -p5671:5671 -p5672:5672 -p15671:15671 -p15672:15672 -p25672:25672 --restart=always -v rabbitmq:/var/lib/rabbitmq --name rabbitmq -d docker.io/rabbitmq
+docker run -p5671:5671 -p5672:5672 -p15671:15671 -p15672:15672 -p25672:25672 --name rabbitMQ -h rabbitMQServer --restart=always -v rabbitmq:/var/lib/rabbitmq --name rabbitmq -d docker.io/rabbitmq
 -------------------- redis
-docker run --name redis -p 6379:6379 --restart=always -d docker.io/redis:latest --requirepass "Jkxy@redis123"
+docker run --name redis -p 6379:6379 --name redisServer -h redisServer --restart=always -d docker.io/redis:latest --requirepass "Jkxy@redis123"
 -------------------- influxdb
 docker run --name influxDB -h influxDBServer -p8086:8086 -p8083:8083 -p2003:2003 --restart=always -v influxdbData:/var/lib/influxdb -d influxdb:1.4.3
 #### ss5
